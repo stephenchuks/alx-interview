bd3ebc990aaf89dfec8a5fda6eaff115a954a4ea
stephenchuks <104122101+stephenchuks@users.noreply.github.com>
2023-07-20 21:12:44 +0100

    Update 0-stats.py

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -1,31 +1,42 @@
 #!/usr/bin/python3
 import sys
 
-def print_stats(file_size, status_codes):
-    print("File size: {}".format(file_size))
-    for code in sorted(status_codes.keys()):
-        if status_codes[code] > 0:
+def print_stats(total_size, status_codes):
+    print("File size: {}".format(total_size))
+    sorted_status_codes = sorted(status_codes.keys())
+    for code in sorted_status_codes:
+        if code.isdigit():
             print("{}: {}".format(code, status_codes[code]))
 
 def main():
-    file_size = 0
-    status_codes = {'200': 0, '301': 0, '400': 0, '401': 0, '403': 0, '404': 0, '405': 0, '500': 0}
-    count = 0
+    total_size = 0
+    status_codes = {}
 
     try:
-        for line in sys.stdin:
-            count += 1
-            data = line.split()
-            if len(data) >= 7:
-                file_size += int(data[-1])
-                status_code = data[-2]
-                if status_code in status_codes:
-                    status_codes[status_code] += 1
-            if count == 10:
-                print_stats(file_size, status_codes)
-                count = 0
+        for line_number, line in enumerate(sys.stdin, 1):
+            parts = line.split()
+            if len(parts) < 9:
+                continue
+
+            ip, date, method, path, protocol, status_code, file_size = parts[:7]
+
+            if not ip.replace('.', '').isdigit() or not status_code.isdigit() or not file_size.isdigit():
+                continue
+
+            status_code = int(status_code)
+            file_size = int(file_size)
+            total_size += file_size
+
+            if status_code in status_codes:
+                status_codes[status_code] += 1
+            else:
+                status_codes[status_code] = 1
+
+            if line_number % 10 == 0:
+                print_stats(total_size, status_codes)
+
     except KeyboardInterrupt:
-        print_stats(file_size, status_codes)
+        print_stats(total_size, status_codes)
         raise
 
 if __name__ == "__main__":
